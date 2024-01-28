95ac9ae6b7afe862a15f9be42218d0cfb5a1152b
Gregory Bacon <gsbacon11@gmail.com>
2024-01-27 21:37:23 -0500

    email and password verification for create account and password reset pages

--- static/index.js
+++ static/index.js
@@ -1,5 +1,4 @@
-
-function validateEmail (email) {
+function validateEmailString (email) {
     email_regex = /^[0-9a-zA-Z]+@[0-9a-zA-Z]+[\.]{1}[0-9a-zA-Z]+[\.]?[0-9a-zA-Z]+$/;
     if(email_regex.test(email)){
         return true;
@@ -7,7 +6,7 @@ function validateEmail (email) {
     return false;
 };
 
-function validatePassword (password) {
+function validatePasswordString (password) {
     // Password cannot contain white spaces
     if (/\s/.test(password)) {
         return false;
@@ -20,53 +19,86 @@ function validatePassword (password) {
 
 }
 
-
-var email_error_shown = false;
-var password_error_shown = false;
-
-function inputValidation () {
-    // Get user input for email and password
-    var email = document.getElementById("login-email").value;
-    var password = document.getElementById("login-password").value;
-    var error = false;
-    // Validate email 
-    var element_login_email = document.getElementById("login-email");
+function verifyEmail() {
+    var email = document.getElementById("input-email").value;
+    var element_input_email = document.getElementById("input-email");
     var element_div_error_email = document.getElementById("div-error-email");
-    if(!validateEmail(email)) {
+    if(!validateEmailString(email)) {
         // Email NOT correct format
-        error = true;
-        if(!email_error_shown) { 
-            element_login_email.style["border-color"] = "red";
-            element_div_error_email.innerHTML =
-            "<label>Please enter a valid email</label>";
-            email_error_shown = true;
-        }
+         element_input_email.style["border-color"] = "red";
+        element_div_error_email.innerHTML =
+        "<label>Please enter a valid email</label>";
+        return false;
     } else {
         // Email good
-        element_login_email.style["border-color"] = "var(--silver_reign)";
+        element_input_email.style["border-color"] = "var(--silver_reign)";
         element_div_error_email.innerHTML = ""
-        email_error_shown = false;
     }
-    // Validate Password
-    var element_login_password = document.getElementById("login-password");
+    return true;
+}
+
+function verifyPassword(){
+    var password = document.getElementById("input-password").value;
+    var element_input_password = document.getElementById("input-password");
     var element_div_error_password = document.getElementById("div-error-password");
-    if(!validatePassword(password)) {
+    if(!validatePasswordString(password)) {
         // Password NOT correct format
-        error = true;
-        if(!password_error_shown){
-            element_login_password.style["border-color"] = "red";
-            element_div_error_password.innerHTML =
-            "<label>Password must be greater than 7 characters with no spaces. Please try again.</label>"
-            password_error_shown = true;
-        }
+        element_input_password.style["border-color"] = "red";
+        element_div_error_password.innerHTML =
+        "<label>Password must be greater than 7 characters with no spaces. Please try again.</label>"
+
+        return false;
     } else {
         // Password good
-        element_login_password.style["border-color"] = "var(--silver_reign)";
+        element_input_password.style["border-color"] = "var(--silver_reign)";
         element_div_error_password.innerHTML = ""
-        password_error_shown = false;
     }
-    // Noe error, redirect to setup duo
-    if (!email_error_shown && !password_error_shown){
+    return true;
+}
+
+function verifyMatchingPasswords () {
+    var div_error_password_retype = document.getElementById("div-error-password_retype");
+    var element_input_password = document.getElementById("input-password");
+    var element_input_password_retype = document.getElementById("input-password_retype");
+    if(element_input_password.value != element_input_password_retype.value){
+        element_input_password_retype.style["border-color"] = "red";
+        div_error_password_retype.innerHTML =
+        "<label>Passwords do not match. Please try again.</label>"
+        return false;
+    } else {
+        // Passwords match
+        element_input_password_retype.style["border-color"] = "var(--silver_reign)";
+        div_error_password_retype.innerHTML = ""
+    }
+    return true;
+}
+
+function onSignIn () {
+    var email_valid = verifyEmail();
+    var password_valid = verifyPassword();
+    if(email_valid && password_valid){
+        email_error_shown = false;
+        password_error_shown = false;
         window.location.href = "./user/user_main.html";
+    }
+}
+
+function onPasswordReset () {
+    if(verifyEmail()){
+        email_error_shown = false;
+        password_error_shown = false;
+        console.log("Send email to reset password");
+    }
+}
+
+function onSignUp () {
+    // Verify email and password
+    var email_valid = verifyEmail();
+    var password_valid = verifyPassword();
+    var passwords_match = verifyMatchingPasswords();
+    if(email_valid && password_valid && passwords_match){
+        email_error_shown = false;
+        password_error_shown = false;
+        console.log("Signed up!");
     }
 }
